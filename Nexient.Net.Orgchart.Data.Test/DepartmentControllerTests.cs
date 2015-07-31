0b2b31c6b7df4836440405368249c85ecd3a7c21
abalkany <abalkany@nexient.com>
2015-07-31 14:33:51 -0400

    Added Department controller test file and Department controller that passes the first test.

---
+++ Nexient.Net.Orgchart.Data.Test/DepartmentControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using Nexient.Net.Orgchart.WebAPI.Controllers;
+using Nexient.Net.Orgchart.Data.Ninject;
+
+namespace Nexient.Net.Orgchart.Data.Test
+{
+    [TestFixture]
+    public class DepartmentControllerTests
+    {
+        private DepartmentController _sut;
+        private Mock<IDepartmentRepository> _repository;
+        private int _departmentId = 111;
+        private string _jobTitleDescription = "a-description";
+        private string _jobTitleDescription2 = "another-description";
+
+        [SetUp]
+        public void Setup()
+        {
+            NinjectBag.InitializeNinject();
+            _repository = new Mock<IDepartmentRepository>();
+            _sut = new DepartmentController(_repository.Object);
+        }
+
+        [Test]
+        public void GetAllDepartmentsCallsGetAllDepartmentsInRepository()
+        {
+            _sut.GetAllDepartments();
+            _repository.Verify(_ => _.GetAllDepartments());
+        }
+    }
+}
---
+++ Orgchart/Controllers/DepartmentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Nexient.Net.Orgchart.Data;
+using Nexient.Net.Orgchart.Data.Ninject;
+using Nexient.Net.Orgchart.Data.NHibernate;
+using Ninject;
+
+namespace Nexient.Net.Orgchart.WebAPI.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository repository)
+        {
+            _departmentRepository = repository;
+        }
+
+        public DepartmentController()
+        {
+            _departmentRepository = NinjectBag.Kernel.Get<IDepartmentRepository>();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllDepartments()
+        {
+            using (var uow = new UnitOfWork())
+            {
+                uow.BeginTransaction();
+                var jobTitles = _departmentRepository.GetAllDepartments();
+                var ret = Json(jobTitles);
+
+                return ret;
+            }
+        }
+
+    }
+}
